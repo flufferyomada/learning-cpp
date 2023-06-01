c7d5b68997b483a492850ed8538c35de8740f216
fluf <74445505+flufferyomada@users.noreply.github.com>
2023-05-31 20:59:57 -0400

    Create inches_to_feet.cpp

---
+++ inches_to_feet.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int inches; 
+inches = 100; 
+
+cout<< inches << " inches = "; 
+cout<< inches / 12 << " feet/foot and "; 
+cout<< inches % 12 << " inches "<<endl;
+
+return 0;
+}
