96f20666bcf0f04406a0e270b60616434583d529
fluf <74445505+flufferyomada@users.noreply.github.com>
2023-05-31 20:55:25 -0400

    Create sizes.cpp

---
+++ sizes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  cout<<"Sizes ::"<<endl;
+  cout<<"###############"<<endl;
+  cout<<"char "<<sizeof(char)<<" bytes"<<endl;
+  cout<<"short "<<sizeof(short)<<" bytes"<<endl;
+  cout<<"int "<<sizeof(int)<<" bytes"<<endl;
+  cout<<"long "<<sizeof(long)<<" bytes"<<endl;
+  cout<<"long long "<<sizeof(long long)<<" bytes"<<endl;
+  cout<<"float "<<sizeof(float)<<" bytes"<<endl;
+  cout<<"double "<<sizeof(double)<<" bytes"<<endl;
+  cout<<"long double "<<sizeof(long double)<<" bytes"<<endl;
+  cout<<"bool "<<sizeof(bool)<<" bytes"<<endl;
+
+    return 0;
+}
